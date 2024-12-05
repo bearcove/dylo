b8afbbc641de3aea0ac4d5c3133090a7d8a007c7
Amos Wenger <amos@bearcove.net>
2024-12-05 19:17:21 +0100

    rename attribute to export

--- con/src/lib.rs
+++ con/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
-pub fn spec(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }

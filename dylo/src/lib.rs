97d873d137b93aff142fd0dcaea231e4f4982408
Amos Wenger <amos@bearcove.net>
2024-12-14 21:30:37 +0100

    Try out unsynn

--- dylo/src/lib.rs
+++ dylo/src/lib.rs
@@ -1,6 +1,25 @@
-use proc_macro::TokenStream;
+use unsynn::*;
+
+unsynn! {
+    keyword Impl = "impl";
+    keyword For = "for";
+
+    struct ImplTraitForStruct {
+        _impl: Impl,
+        trait_name: Ident,
+        _for: For,
+        struct_name: Ident,
+        body: BraceGroupContaining<TokenStream>,
+    }
+}
 
 #[proc_macro_attribute]
-pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+pub fn export(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let item = TokenStream::from(item);
+    let mut token_iter = item.to_token_iter();
+    let ast = ImplTraitForStruct::parse(&mut token_iter).unwrap();
+    panic!("{:?}", ast);
 }
